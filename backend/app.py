8ab2f43b9a8d92a651e9a29f044e91006868ab24
Pushkar Nagarmote <pushkarnagarmote712@gmail.com>
2026-06-18 14:18:42 +0530

    Render deployment fix

--- backend/app.py
+++ backend/app.py
@@ -226,4 +226,5 @@ def reset_password():
     return jsonify({"message": "Password updated!"})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
